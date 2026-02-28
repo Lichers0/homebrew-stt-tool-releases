67498952206185ac0de3c62f3e67a7700b89e074
Denis <romodanov.denis@gmail.com>
2026-02-28 18:44:06 +0300

    fix: add postflight to remove quarantine, update repo URLs

--- Casks/stt-tool.rb
+++ Casks/stt-tool.rb
@@ -2,10 +2,15 @@ cask "stt-tool" do
   version "1.5.1"
   sha256 "708859455a511828b6cbc48ea8d3042ca68e41e343c1bbf8a31209894c396a33"
 
-  url "https://github.com/Lichers0/stt-tool-releases/releases/download/v1.5.1/STTTool.zip"
+  url "https://github.com/Lichers0/homebrew-stt-tool-releases/releases/download/v#{version}/STTTool.zip"
   name "STT Tool"
   desc "Speech-to-text tool with global hotkey"
-  homepage "https://github.com/Lichers0/stt-tool-releases"
+  homepage "https://github.com/Lichers0/homebrew-stt-tool-releases"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+      args: ["-rd", "com.apple.quarantine", "#{appdir}/STTTool.app"]
+  end
 
   app "STTTool.app"
 
